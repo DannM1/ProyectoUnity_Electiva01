e3d162ae2f7901f4eda8a279da45622b8c8f7103
DannM1 <123668643+DannM1@users.noreply.github.com>
2025-07-27 09:55:09 -0500

    Inicio del script RegistroGrupo.cs por Daniela

---
+++ ProyectoUnityElectiva01/Assets/RegistroGrupo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegistroGrupo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Nombre completo: Daniela Porras Matiz
+        // Fecha: 27/07/2024
+        // Hora: 9:39
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
