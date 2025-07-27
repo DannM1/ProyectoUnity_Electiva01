99434c8eed460105aebb00a226c4e6bfc3a76650
lm-duque <123669002+lm-duque@users.noreply.github.com>
2025-07-27 18:47:51 -0500

    Update RegistroGrupo.cs

--- ProyectoUnityElectiva01/Assets/RegistroGrupo.cs
+++ ProyectoUnityElectiva01/Assets/RegistroGrupo.cs
@@ -22,6 +22,10 @@ public class RegistroGrupo : MonoBehaviour
         // Nombre completo: Daniela Porras Matiz
         // Fecha: 27/07/2024
         // Hora: 18:23
+
+        // Nombre completo: Laura Manuela Calderon Duque
+        // Fecha: 27/07/2024
+        // Hora: 18:47
     }
     
     // Update is called once per frame
